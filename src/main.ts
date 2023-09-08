e53bdd191cbfdfa69745908426aaa1c3059e0a91
rai <mj82xx@gmail.com>
2023-09-08 18:50:48 +0900

    step 4

--- src/main.ts
+++ src/main.ts
@@ -53,25 +53,111 @@ export const createTextElement = (text: string): TextElement => ({
   },
 });
 
-export const render = (
-  element: MyReactElement,
-  container: HTMLElement | Text
-) => {
-  const dom =
-    element.kind === "text"
-      ? document.createTextNode(element.props.nodeValue)
-      : document.createElement(element.type);
+// Rendering
 
-  Object.keys(element.props)
+const createDom = (fiber: Fiber) => {
+  const dom =
+    fiber.kind === "text"
+      ? document.createTextNode(fiber.props.nodeValue)
+      : document.createElement(fiber.type);
+
+  Object.keys(fiber.props)
     .filter((key) => key !== "children")
     .forEach((name) => {
       // @ts-ignore: this is safe because name is a key of props
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
 
-  element.props.children.forEach((child) => render(child, dom));
+  return dom;
+};
 
-  container.appendChild(dom);
+type Fiber = {
+  dom: HTMLElement | Text | undefined;
+  child?: Fiber | undefined;
+  parent?: Fiber | undefined;
+  sibling?: Fiber | undefined;
+} & MyReactElement;
+
+let nextUnitOfWork: Fiber | undefined = undefined;
+
+export const render = (element: MyReactElement, container: HTMLElement) => {
+  nextUnitOfWork = {
+    kind: "node",
+    type: element.type,
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+};
+
+const workLoop: IdleRequestCallback = (deadline) => {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+};
+
+requestIdleCallback(workLoop);
+
+const performUnitOfWork = (fiber: Fiber) => {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  fiber.parent?.dom?.appendChild(fiber.dom);
+
+  // create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling: Fiber | undefined = undefined;
+
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber: Fiber =
+      element.kind === "text"
+        ? {
+            kind: "text",
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: undefined,
+          }
+        : {
+            kind: "node",
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: undefined,
+          };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      // because we are iterating over the children, we can assume that the previous sibling exists
+      prevSibling!.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // search for next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent!;
+  }
 };
 
 export default {
